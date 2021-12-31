95b040ffb2e53076d0e70bd1057f36581726ec0a
Maksim Repko <maxrepko@yahoo.com>
2021-12-30 18:55:49 -0800

    Two_Sum_V2
    Updated Two Sum solution.
    Improved Runtime: 103 ms > 5 ms
    Similar Memory Usage: 39.2 MB > 39.4 MB

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,20 +1,46 @@
-// Runtime: 103 ms
-// Memory Usage: 39.2 MB
+// Runtime: 5 ms
+// Memory Usage: 39.4 MB
+
+import java.util.*;
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int x = 0;
-        int y = 0;
-        while (true) {
-            if ((x != y) && (nums[x] + nums[y] == target)) {
-                int[] temp = {x, y};
-                return temp;
-            }
-            if (y == nums.length - 1) {
-                y = 0;
-                x++;
-            }
-            else y++;
+    public static int[] twoSum(int[] nums, int target) {
+        int hmCount = 10;
+        HashMap<Integer, List<Integer>> hm = createHash(nums, hmCount);
+        return getAnswer(hm, nums, target, hmCount);
+    }
+
+    private static HashMap<Integer, List<Integer>> createHash(int[] nums, int hmCount) {
+        HashMap<Integer, List<Integer>> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = nums[i] / hmCount;
+            List<Integer> values;
+            if (hm.containsKey(key)) {values = hm.get(key);}
+            else {values = new ArrayList<>();}
+            values.add(i);
+            hm.put(key, values);
         }
+        return hm;
+    }
+
+    private static int[] getAnswer (HashMap<Integer, List<Integer>> hm, int[] nums, int target, int hmCount) {
+        int difference, key;
+        List<Integer> temp;
+        for (int i = 0; i < nums.length; i++) {
+            difference = target - nums[i];
+            key = difference / hmCount;
+            if (hm.containsKey(key)) {
+                temp = hm.get(key);
+                for (int j = 0; j < temp.size(); j++) {
+                    boolean match = false;
+                    int testLocation = temp.get(j);
+                    int testValue = nums[testLocation];
+                    if (testValue == difference && i != j) {
+                        return new int[]{i, testLocation};
+                    }
+                }
+            }
+        }
+        return new int[]{0,target};
     }
 }
